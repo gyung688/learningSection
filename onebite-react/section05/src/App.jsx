be8e1e7e0d8c46503ec2d06fe52982c4686937c0
gyung688@gmail.com <gyung688@gmail.com>
2025-02-20 23:58:07 -0800

    react-useState

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -5,14 +5,33 @@ function App() {
   // 이런 함수를 component라고 부름
 
   function App(){
-    const state = useState();
-    console.log(state);
-  }
-  return (
-    <>
-    </>
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF");
+    console.log(count);
 
-  )
+    return (
+      <>
+        <div>
+          <h1>{light}
+            <button onClick={() => {
+                setLight(light === "ON" ? "OFF" : "ON");
+              }}
+            >
+              {light === "ON" ? "OFF" : "ON"}
+            </button>
+          </h1>
+        </div>
+        <h1>{count}</h1>
+        <button onClick={()=>{
+          setCount(count + 1);
+        }}
+        >
+         + 
+        </button>
+      </>
+  
+    )
+  }
 }
 
 export default App
